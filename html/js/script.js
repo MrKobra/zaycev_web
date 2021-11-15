f914c9ab62acfb3ab31832ef8f9ec6876c0243e5
Mr-Kobra <red.kras.2013@gmail.com>
2021-11-15 23:11:37 +0300

    Add portfolio block

--- html/js/script.js
+++ html/js/script.js
@@ -20,6 +20,39 @@ $(document).ready(function(){
         slidesToShow: 6,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 1000
+        autoplaySpeed: 2000
     })
+
+    // Команда
+    $('.team-nav a').on('click', function(e){
+        e.preventDefault();
+        var href = $(this).attr('href');
+        $('.team-nav li').removeClass('active');
+        $(this).parent().addClass('active');
+        $('.team-block').removeClass('animate__backInRight show');
+        $(href).addClass('animate__backInRight show');
+    })
+
+    function setMaxTeamHeight() {
+        var max = 0;
+        var flag = false;
+        $('.team-block').each(function(){
+            if($(this).hasClass('show')) {
+                flag = true;
+            } else {
+                $(this).addClass('show');
+            }
+            if(max < $(this).outerHeight()) {
+                max = $(this).outerHeight();
+            }
+            if(!flag) {
+                $(this).removeClass('show');
+            }
+            flag = false;
+        })
+        if(max > $('.team-nav').outerHeight()) {
+            $('.team-container').css('height', max);
+        }
+    }
+    setMaxTeamHeight();
 })
